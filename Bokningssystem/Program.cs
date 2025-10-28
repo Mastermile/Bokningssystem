7eb626f803be269e63b6b1d355de9ad94d6d6899
Mastermile <45874996+Mastermile@users.noreply.github.com>
2025-10-28 02:54:31 +0100

    Add project files.

---
+++ Bokningssystem/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Bokningssystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine($"[1] Boka Sal");
+                Console.WriteLine($"[2] Bokningar");
+                Console.WriteLine($"[3] Lokaler");
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
+
+interface IBookable //interface för klasser som ska kunna bokas
+{
+    void StartTid();
+    void SlutTid();
+}
+
+class Lokal : IBookable//Base class
+{
+
+    
+    public void StartTid()
+    {
+        throw new NotImplementedException();
+    }
+    public void SlutTid()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Sal : Lokal
+{
+
+}
+
+class Grupprum : Lokal
+{
+
+}
